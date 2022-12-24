4413bc756bd77716016296736a0052a71b82c951
AzureAD\MarkoManic <kursqa@gmail.com>
2022-12-24 11:57:16 +0100

    Initial Commit

---
+++ AutomationExerciseFramework/Helpers/TestConstants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomationExerciseFramework.Helpers
+{
+    public static class TestConstants
+    {
+        //existing user data
+        public const string Username = "kursqa+9@gmail.com";
+        public const string Password = "Gorandrulic999";
+
+        //new user data
+        public const string FirstName = "Marko";
+        public const string LastName = "Manic";
+        public const string Address = "Bulevar Milutina Milankovica 7";
+        public const string Country = "Canada";
+        public const string State = "Ontario";
+        public const string City = "Montreal";
+        public const string ZipCode = "10010";
+        public const string Phone = "0800100100";
+    }
+}
